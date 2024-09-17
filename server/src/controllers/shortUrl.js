a4d4306b3d6dc94ceb9050458f7c386fb3eb2100
sabarish <vbsabarishsriram@gmail.com>
2024-09-17 16:58:50 +0530

    feat: deleteUrl API

--- server/src/controllers/shortUrl.js
+++ server/src/controllers/shortUrl.js
@@ -19,8 +19,8 @@ export const createUrl = async (req, res) => {
 export const getAllUrl = async (req, res) => {
   try {
     const shortUrl = await urlModel.find();
-    if (shortUrl.length < 0) {
-      res.status(404).send({ message: "Url not Found" });
+    if (shortUrl.length == 0) {
+      res.status(404).send({ message: "Full Url's not Found" });
     } else {
       res.send(shortUrl);
     }
@@ -31,6 +31,16 @@ export const getAllUrl = async (req, res) => {
 
 export const getUrl = async (req, res) => {
   try {
+    const { id } = req.params;
+    console.log(id);
+    const shortUrl = await urlModel.findOne({ shortUrl: id });
+    if (!shortUrl) {
+      res.status(404).send({ message: "Url does not exist" });
+    } else {
+      shortUrl.click++;
+      res.redirect(shortUrl.fullUrl);
+      shortUrl.save();
+    }
   } catch (error) {
     res.status(500).send("Something went wrong");
   }
@@ -38,6 +48,11 @@ export const getUrl = async (req, res) => {
 
 export const deleteUrl = async (req, res) => {
   try {
+    const { id } = req.params;
+    const deleteUrl = await urlModel.findByIdAndDelete(id);
+    if(deleteUrl){
+      res.status(201).send("Data deleted succesfully")
+    }
   } catch (error) {
     res.status(500).send("Something went wrong");
   }
