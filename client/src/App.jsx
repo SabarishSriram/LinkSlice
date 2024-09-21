5f93c42a3080a70cd328dc19a722601fd5705d3e
sabarish <vbsabarishsriram@gmail.com>
2024-09-22 00:14:26 +0530

    feat: init shadcn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import ParticleBackground from './components/ParticleBackground'
 
 
 
 function App() {
   return (
     <div>
-
+      <ParticleBackground/>
+      <Navbar/>
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex'>
+      <h1 className='text-2xl font-bold'>Linkslice</h1>
+    </div>
   )
 }
 
